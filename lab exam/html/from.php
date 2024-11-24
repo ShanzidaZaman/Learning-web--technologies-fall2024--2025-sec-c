ba5ecd0d1387e4a02fbbb74c7157eee995c95183
ShanzidaZaman <shanzidazamanedu@gmail.com>
2024-11-24 15:38:40 +0600

    Add files via upload

---
+++ lab exam/html/from.php
@@ -0,0 +1,26 @@
+<?php include "../control_reg/control_name.php"; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Register Page</title>
+</head>
+<body>
+
+    <form method="post" action="">
+        <label for="name">NAME:</label>
+        <input type="text" id="name" name="name"><br><br>
+
+        <label for="email">EMAIL:</label>
+        <input type="email" id="email" name="email"><br><br>
+
+        <label for="degree">DEGREE:</label>
+        <input type="text" id="degree" name="degree"><br><br>
+
+        <label for="blood">BLOOD GROUP:</label>
+        <input type="text" id="blood" name="blood"><br><br>
+
+        <button type="submit">Submit</button>
+    </form>
+
+</body>
+</html>
